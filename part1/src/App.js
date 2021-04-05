ca9df3d5d7bba1f26e734f528878ce283e82e07b
darkgraycat <jburyanov@gmail.com>
2021-04-05 18:40:10 +0300

    part 1c done

--- part1/src/App.js
+++ part1/src/App.js
@@ -17,16 +17,8 @@ const App = () => {
   )
 }
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
 
 export default App
