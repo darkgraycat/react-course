6c14e753682f180271fb463f328711db81cc69ca
darkgraycat <jburyanov@gmail.com>
2021-04-06 01:56:56 +0300

    get ready to do ex 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,70 +1,19 @@
 import React, { useState } from 'react'
 
+const Display = ({ value }) => <div>{value}</div>
+const Button = ({ handler, text }) => <button onClick={handler}>{text}</button>
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
+  const [value, setValue] = useState(10)
+  const setToValue = value => setValue(value)
   return (
-    <div>
-      <Display left={left} right={right} />
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      <History allClicks={allClicks} />
-      <Hello />
-      <EventTest />
-    </div>
+    <>
+      <Display value={value} />
+      <Button handler={() => setToValue(1000)} text="thousand" />
+      <Button handler={() => setToValue(0)} text="reset" />
+      <Button handler={() => setToValue(value + 1)} text="increment" />
+    </>
   )
 }
-
-const History = ({ allClicks }) => {
-  return (
-    <div>
-      {
-        allClicks.length === 0 ?
-          `the app is used by pressing the buttons` :
-          `button press history: ${allClicks.join(' ')}`
-      }
-    </div>
-  )
-}
-
-const Hello = () => {
-  const hello = (who) => () => console.log('hello', who)
-  return (
-    <div>
-      <Button handleClick={hello('world')} text='to world' />
-      <Button handleClick={hello('react')} text='to react' />
-      <Button handleClick={hello('people')} text='to people' />
-    </div>
-  )
-}
-
-const EventTest = () => {
-  const [value, setValue] = useState(0)
-  const setToValue = (value) => () => setValue(value)
-  return (
-    <div>
-      {value}
-      <br />
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(100)}>hundred</button>
-      <button onClick={setToValue(value + 1)}>+1</button>
-    </div >
-  )
-}
-
-const Display = ({ left, right }) => <div>{left}:{right}</div>
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
 export default App
