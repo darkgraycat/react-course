271ffdcadbeb99d45d46d8c4e03f725a06a7b1d1
darkgraycat <jburyanov@gmail.com>
2021-04-05 22:04:17 +0300

    another click counter

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
-
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display left={clicks.left} right={clicks.right} />
+      <Display left={left} right={right} />
       <Button handleClick={handleLeftClick} text='left' />
       <Button handleClick={handleRightClick} text='right' />
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
