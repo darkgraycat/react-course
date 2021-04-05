402bd5c08348baafaaa869ec26f99f6243f2d8a8
darkgraycat <jburyanov@gmail.com>
2021-04-05 17:18:11 +0300

    counter created

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,7 +3,14 @@ import App from './App'
 
 let counter = 1
 
-ReactDOM.render(
-  <App counter={counter} />,
-  document.getElementById('root')
-)
+const refresh = () => {
+  ReactDOM.render(
+    <App counter={counter} />,
+    document.getElementById('root')
+  )
+}
+
+setInterval(() => {
+  refresh()
+  counter++
+}, 1000)
