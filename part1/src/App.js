c3cd37e2dc53e972164315fc49c9478abe7c698e
darkgraycat <jburyanov@gmail.com>
2021-04-13 12:05:42 +0300

    part 1c completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -8,18 +8,25 @@ const Display = props => {
   )
 }
 
+const Button = props => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
       <Display counter={counter} />
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={decreaseByOne} text='minus' />
+      <Button handleClick={setToZero} text='zero' />
     </div>
   )
 }
